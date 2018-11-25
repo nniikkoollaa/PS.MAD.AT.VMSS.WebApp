6141154b1998c9791b0e922703cfcbce4c5ed26e
Nikola Milanovic <nniikkoollaa@gmail.com>
2018-11-25 19:10:29 +0100

    Add project files.

---
+++ PS.MAD.AT.VMSS.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PS.MAD.AT.VMSS.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
